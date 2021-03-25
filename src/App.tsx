426e2cb14209f5a6a4bd9166e473b34aeb2967c2
DESKTOP-UD5VQCF\thiti <thiti.adev@gmail.com>
2021-03-25 22:45:18 +0700

    set styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+import { Wrapper } from './App.styles';
 
 const App = () => {
   return (
