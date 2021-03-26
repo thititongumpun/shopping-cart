78b6e4ab57508ad01aed9519f81daacf5cce4f0a
Thiti <thiti.adev@gmail.com>
2021-03-26 20:24:20 +0700

    Add Cart

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton} from './App.styles';
 
 export interface CartItemType {
   id: number;
@@ -26,13 +26,15 @@ const getProducts = async (): Promise<CartItemType[]> => {
 } 
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     'products',
     getProducts
   );
   console.log(data);
   
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => null;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -44,6 +46,15 @@ const App = () => {
   return (
     <Wrapper>
       <Grid container spacing={3}>
+
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart Here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
             <Item item={item} handleAddToCart={handleAddToCart} />
